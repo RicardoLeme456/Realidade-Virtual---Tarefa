be09293a0eaa7f74f093e26fa450ef6ce3e1308c
RicardoLeme456 <69600786+RicardoLeme456@users.noreply.github.com>
2021-04-02 10:07:03 -0300

    Finished

---
+++ Realidade Virtual - Tarefa/Assets/Rotate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnumAxis
+{
+    x,
+    y,
+    z,
+}
+
+public class Rotate : MonoBehaviour
+{
+    public EnumAxis axis;
+    public float speed;
+
+    private void Update()
+    {
+        switch (axis)
+        {
+            case EnumAxis.x:
+                transform.Rotate(speed, 0, 0);
+                break;
+            case EnumAxis.y:
+                transform.Rotate(0, speed, 0);
+                break;
+            case EnumAxis.z:
+                transform.Rotate(0, 0, speed);
+                break;
+        }
+    }
+
+}
