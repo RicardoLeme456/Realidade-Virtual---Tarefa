c29ccd5cbfd31b81ee96ca7fdf2131de3f6b075e
RicardoLeme456 <69600786+RicardoLeme456@users.noreply.github.com>
2021-04-08 08:18:48 -0300

    Update

---
+++ Aulas/Assets/Script/Variaveis.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variaveis : MonoBehaviour
+{
+    int numero1, numero2, resultadoSub;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numero1 = 10;
+        numero2 = 30;
+        Soma();
+        Multiplicação(numero1, numero2);
+        resultadoSub = sub(numero1, numero2);
+        Debug.Log(resultadoSub);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void Soma()
+    {
+        int resultado;
+        resultado = numero1 + numero2;
+        Debug.Log(resultado);
+    }
+
+    void Multiplicação(int valorA, int valorB)
+    {
+        int resultado;
+        resultado = valorA * valorB;
+        Debug.Log(resultado);
+    }
+    int sub(int valorA, int valorB)
+    {
+        int resultado = valorB - valorA;
+        return resultado;
+    }
+}
---
+++ Jogo do Rato/Assets/EventHandler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EventHandler : MonoBehaviour
+{
+    public InputField ifForward;
+    public InputField ifBackward;
+    public InputField ifJump;
+    public InputField ifAttack;
+    public Slider     slVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadPreferences();
+    }
+
+    public void LoadPreferences()
+    {
+        ifForward.text  = PlayerPrefs.GetString("ForwardKey" , "D");
+        ifBackward.text = PlayerPrefs.GetString("BackwardKey", "A");
+        ifJump.text     = PlayerPrefs.GetString("JumpKey"    , "J");
+        ifAttack.text   = PlayerPrefs.GetString("AttackKey"  , "K");
+        slVolume.value  = PlayerPrefs.GetInt   ("VolumeKey"  , 50);
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.DeleteAll();
+        LoadPreferences();
+    }
+
+    public void SavePreferences()
+    {
+        PlayerPrefs.SetString("ForwardKey" , ifForward.text);
+        PlayerPrefs.SetString("BackwardKey", ifBackward.text);
+        PlayerPrefs.SetString("JumpKey"    , ifJump.text);
+        PlayerPrefs.SetString("AttackKey"  , ifAttack.text);
+        PlayerPrefs.SetInt   ("VolumeKey"  , (int)slVolume.value);
+    }
+
+   
+}
